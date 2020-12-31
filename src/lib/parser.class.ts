c410d34f94b698eb6573bbe8f8fbe3df88bbe80a
Luc Freyermuth <lucfreyermuth@gmail.com>
2020-12-31 02:44:03 +0100

    default options

--- src/lib/parser.class.ts
+++ src/lib/parser.class.ts
@@ -1,6 +1,6 @@
 import { Parser as GrammarParser } from './grammar-parser/grammar-parser';
 import { operateJavascriptly, operateSafely } from './operate/operate';
-import { ParserOptions } from './options';
+import { defaultOptions, ParserOptions } from './options';
 import { oppositeNumber, trimEdges } from './utils.functions';
 
 export type ParsedFunction = (variables: { [variable: string]: any }) => any;
@@ -8,7 +8,11 @@ export type ParsedFunction = (variables: { [variable: string]: any }) => any;
 export class Parser {
   private grammarParser: any;
 
-  constructor(options: ParserOptions) {
+  constructor(options: Partial<ParserOptions> = {}) {
+    options = {
+      ...defaultOptions,
+      ...options,
+    };
     this.grammarParser = new GrammarParser();
     this.grammarParser.yy = {
       oppositeNumber,
